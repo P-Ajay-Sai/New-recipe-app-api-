4e5f8a6ac5a70920f31b3b181cb45ba22979e305
P-Ajay-Sai <117829349+P-Ajay-Sai@users.noreply.github.com>
2022-12-20 12:52:48 +0530

    urls has only admin

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -44,3 +45,10 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
